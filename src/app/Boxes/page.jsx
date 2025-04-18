4e44b9b3fdfebadb711a4e6f7c275f51dc868ea1
Irfan Raza <irfanraz67@gmail.com>
2025-04-19 04:21:26 +0500

    new changes

--- src/app/Boxes/page.jsx
+++ src/app/Boxes/page.jsx
@@ -1,30 +1,74 @@
+// "use client";
+// import React from "react";
+// import Image from "next/image";
+
+// const BoxGrid = () => {
+//   const boxes = [
+//     { img: "/Boxes/LOGO1.png", text: "Sectors and Industries" },
+//     { img: "/Boxes/LOGO2.png", text: "Business Description" },
+//     { img: "/Boxes/LOGO3.png", text: "Locations" },
+//     { img: "/Boxes/LOGO4.png", text: "Business Cycles and Backing Status" },
+//     { img: "/Boxes/LOGO5.png", text: "Deals and Investors" },
+//     { img: "/Boxes/LOGO6.png", text: "Financial Metrics" },
+//     { img: "/Boxes/LOGO7.png", text: "Intellectual Property" },
+//   ];
+
+//   return (
+//     <div className="borderColorUp ">
+//       <p className="mx-8   boxHeader">Explore Company Criteria below</p>
+
+//       <div className="flex  items-center flex-wrap  justify-evenly pt-14 px-10">
+//         {boxes.map((box, index) => (
+//           <div key={index} className="box-grid-box">
+//             <p className="box-grid-text text-center px-1">{box.text}</p>
+//               <Image
+//                 src={box.img}
+//                 alt={`Box ${index + 1}`}
+//                 className="box-grid-image"
+//                 width={100}
+//                 height={100}
+//               />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default BoxGrid;
+
+
+
+
+
+
+
+
+
+
 "use client";
 import React from "react";
-import { IoEarth } from "react-icons/io5";
 import Image from "next/image";
 
 const BoxGrid = () => {
   const boxes = [
-    { img: "/Boxes/logo1.png", text: "Sectors and Industries" },
-    { img: "/Boxes/logo2.png", text: "Business Description" },
-    { img: "/Boxes/logo3.png", text: "Locations" },
-    { img: "/Boxes/logo4.png", text: "Business Cycles and Backing Status" },
-    { img: "/Boxes/logo5.png", text: "Deals and Investors" },
-    { img: "/Boxes/logo6.png", text: "Financial Metrics" },
-    { img: "/Boxes/logo7.png", text: "Intellectual Property" },
+    { img: "/Boxes/LOGO1.png", text: "Sectors and Industries" },
+    { img: "/Boxes/LOGO2.png", text: "Business Description" },
+    { img: "/Boxes/LOGO3.png", text: "Locations" },
+    { img: "/Boxes/LOGO4.png", text: "Business Cycles and Backing Status" },
+    { img: "/Boxes/LOGO5.png", text: "Deals and Investors" },
+    { img: "/Boxes/LOGO6.png", text: "Financial Metrics" },
+    { img: "/Boxes/LOGO7.png", text: "Intellectual Property" },
   ];
 
   return (
-    <div className="borderColorUp ">
-      <p className="mx-8 text-2xl">Explore Company Criteria below</p>
+    <div className=" xl:my-0 my-10">
+      <p className="mx-8   boxHeader">Explore Company Criteria below</p>
 
-      <div className="flex gap-12 items-center flex-wrap p-8">
+      <div className="flex  items-center flex-wrap space-x-0 xl:space-x-3 xl:space-y-0 space-y-4 justify-evenly pt-8 px-4 sm:pt-10 sm:px-6 md:pt-12 md:px-8 lg:pt-14 lg:px-10 ">
         {boxes.map((box, index) => (
-          <div key={index} className="box-grid-box">
-            <p className="box-grid-text text-center px-2">{box.text}</p>
-            {index === 2 ? (
-              <IoEarth size={90} color="rgba(0, 0, 0, 1)" />
-            ) : (
+          <div key={index} className="box-grid-box ">
+            <p className="box-grid-text text-center px-1">{box.text}</p>
               <Image
                 src={box.img}
                 alt={`Box ${index + 1}`}
@@ -32,7 +76,6 @@ const BoxGrid = () => {
                 width={100}
                 height={100}
               />
-            )}
           </div>
         ))}
       </div>
@@ -41,3 +84,4 @@ const BoxGrid = () => {
 };
 
 export default BoxGrid;
+
--- src/app/Criteria/page.jsx
+++ src/app/Criteria/page.jsx
@@ -127,98 +127,204 @@
 //   );
 // }
 
+
+
+
+
+
+
+
+
+
+
+// "use client";
+// import { useState } from "react";
+// import { FaArrowDown, FaCaretDown, FaTimes } from "react-icons/fa";
+// import { GoSearch } from "react-icons/go";
+// import { RiArrowUpDownLine } from "react-icons/ri";
+// import { HiArrowSmallDown, HiArrowUp } from "react-icons/hi2";
+// import { IoIosArrowRoundDown } from "react-icons/io";
+// import { IoIosArrowRoundUp } from "react-icons/io";
+
+// export default function Home() {
+//   const [newCriteria, setNewCriteria] = useState("");
+
+//   const removeItem = (id) => {
+//     console.log(`Remove item with id: ${id}`);
+//   };
+
+//   return (
+//     <div className="bg-black pl-19.5 pr-55 pt-12 textCriteria space-y-3 w-3/4 text-white ">
+//       <h2 className="tracking-widest Criteria mb-4 pl-5">CRITERIA</h2>
+//       {Array.from({ length: 3 }, (x, i) => (
+//         <CriteriaRow key={i} index={i + 1} />
+//       ))}
+//       <div className="flex items-center justify-between py-2 ">
+//         <div className="addCriteria flex items-center gap-5 pl-23">
+//           <div className=" text-center ">Add Criteria</div>
+//           <div className="flex-1 flex items-center border-b borderColorCriteria py-2 ">
+//             <input
+//               type="text"
+//               value={newCriteria}
+//               onChange={(e) => setNewCriteria(e.target.value)}
+//               placeholder="Add Search Criteria or Add Keywords"
+//               className=" flex-1 px-4"
+//             />
+//             <GoSearch />
+//           </div>
+//         </div>
+//         <div className="pl-4">
+//           <button className="button px-3 py-1 text-sm">ADD</button>
+//         </div>
+//       </div>
+//       <div className="flex justify-end py-2">
+//         <button className="button px-3 py-1 text-sm w-full sm:w-auto">
+//           RUN SCREEN
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function CriteriaRow({ index, removeItem }) {
+//   const criterions = [
+//     "Industry Classification In (Primary) Real Estate; Health Care",
+//     "Geography In United States and Canada",
+//     "Company Type In Public Company",
+//   ];
+
+//   return (
+//     <div className="flex items-center border-b borderColorCriteria py-1 pl-5 gap-8">
+//       <div className="flex items-center gap-2  ">
+//         <span className="fontInter">{index}</span>
+//         <p className=" text-2xl ">{index === 3 ? "" : "["}</p>{" "}
+//       </div>
+//       <div className={`flex items-center gap-4 ${index === 3 ? "ml-2" : ""} ${index === 1 ? "ml-1.5" : ""}`}>
+//         {index === 1 && <IoIosArrowRoundDown className="text-3xl  " />}
+//         {index === 2 && <span className=" flex -gap-10 text-xl"><IoIosArrowRoundDown className="text-  " /><IoIosArrowRoundUp className="relative -left-3 -top-2" /></span>}
+//         {index === 3 && <IoIosArrowRoundUp className="text-2xl" />}
+
+//         <span className={`w-5  ${index === 3 ? "ml-4.5" : ""}`}>{index === 1 ? <p className=""></p> : "And"}</span>
+//       </div>
+//       <div className="relative inline-block gap-0 ">
+//         {index === 1 ? (
+//           <FaCaretDown className="top-1 absolute pointer-events-none hidden " />
+//         ) : (
+//           <FaCaretDown className="top-1 absolute pointer-events-none " />
+//         )}
+//         <select
+//           defaultValue={criterions[index - 1]}
+//           className="appearance-none pl-8 bg-none"
+//         >
+//           {criterions.map((criterion, i) => (
+//             <option className={`${index === 1 ? "px-2" : ""}`} key={i} value={criterion}>
+//               {criterion}
+//             </option>
+//           ))}
+//         </select>
+//       </div>
+//       <div className="grow flex gap-2 justify-end items-center ">
+//         <span>
+//           {index === 1 && (
+//             <p className="">
+//               <span className="  px-1 opacity-65">{">  "}</span> 250,000
+//             </p>
+//           )}
+//           {index === 2 && "214,712"}
+//           {index === 3 && "3,609"}
+//         </span>{" "}
+//         <button onClick={() => removeItem(1)}>
+//           <FaTimes />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 "use client";
-import { useState } from "react";
+import {  useState } from "react";
 import { FaArrowDown, FaCaretDown, FaTimes } from "react-icons/fa";
 import { GoSearch } from "react-icons/go";
 import { RiArrowUpDownLine } from "react-icons/ri";
 import { HiArrowSmallDown, HiArrowUp } from "react-icons/hi2";
+import { IoIosArrowRoundDown } from "react-icons/io";
+import { IoIosArrowRoundUp } from "react-icons/io";
 
 export default function Home() {
   const [newCriteria, setNewCriteria] = useState("");
 
+  
   const removeItem = (id) => {
     console.log(`Remove item with id: ${id}`);
   };
 
   return (
-    <div className="bg-black p-8 space-y-4 w-3/4 text-white criteria">
-      <h2 className="tracking-widest mb-4">CRITERIA</h2>
+    <div className="  px-10 xl:pl-19.5 xl:pr-55 pt-12 textCriteria space-y-3 w-full xl:w-3/4 text-white ">
 
-      {/* Row 1 */}
+
+   {/* <div className="bg-black w-full sm:w-11/12 md:w-4/5 lg:w-3/4 px-4 sm:px-8 pt-8 textCriteria space-y-3 text-white">  */}
+
+      <h2 className=" Criteria mb-4 pl-0 sm:pl-5">CRITERIA</h2>
       {Array.from({ length: 3 }, (x, i) => (
         <CriteriaRow key={i} index={i + 1} />
       ))}
 
-      {/* Row 2 */}
-      {/* <div className="flex items-center border-b border-gray-700 py-2">
-        <div className="w-8 flex justify-center items-center">
-          <span>2</span>
-          <p className="relative -top-4 text-3xl font-medium left-2">{"["}</p>
-        </div>
-        <div className="w-12 flex items-center gap-6 relative left-2">
-          <RiArrowUpDownLine />
-          <span>And</span>
-        </div>
-        <div className="flex-1 px-2 relative left-2">
-          <div className="flex items-center gap-1">
-            <IoMdArrowDropdown />
-            <span>Geography In United States and Canada</span>
+
+<div className="flex flex-col sm:flex-row items-center justify-between py-2 gap-4">
+        <div className="w-full lg:w-auto addCriteria flex flex-col lg:flex-row items-center gap-4 lg:pl-6">
+          <div className="text-center sm:text-left hidden xl:block">Add Criteria</div>
+          <div className="flex-1 w-full flex items-center border-b borderColorCriteria py-2">
+            <input
+              type="text"
+              value={newCriteria}
+              onChange={(e) => setNewCriteria(e.target.value)}
+              placeholder="Add Search Criteria or Add Keywords"
+              className="flex-1 px-4 bg-transparent outline-none"
+            />
+            <GoSearch />
           </div>
         </div>
-        <div className="w-1/4 flex justify-end items-center">
-          <span className="text-gray-400">214,712</span>
-          <FaTimes className="cursor-pointer" onClick={() => removeItem(2)} />
-        </div>
-      </div> */}
-
-      {/* Row 3 */}
-      {/* <div className="flex items-center border-b border-gray-700 py-2">
-        <div className="w-8 flex justify-center items-center">
-          <span>3</span>
-        </div>
-        <div className="w-12 flex items-center gap-6 relative left-2">
-          <HiArrowUp />
-          <span>And</span>
-        </div>
-        <div className="flex-1 px-2">
-          <div className="flex items-center gap-1">
-            <IoMdArrowDropdown />
-            <span>Company Type In Public Company</span>
-          </div>
-        </div>
-        <div className="w-1/4 flex justify-end items-center">
-          <span className="text-gray-400">3,609</span>
-          <FaTimes className="cursor-pointer" onClick={() => removeItem(3)} />
-        </div>
-      </div> */}
-
-      {/* Add Criteria */}
-      <div className="flex items-center py-2">
-        <div className="w-44 text-center text-sm">Add Criteria</div>
-        <div className="flex-1 flex items-center gap-2 px-4">
-          <input
-            type="text"
-            value={newCriteria}
-            onChange={(e) => setNewCriteria(e.target.value)}
-            placeholder="Add Search Criteria or Add Keywords"
-            className="bg-black border-b border-gray-600 p-2 outline-none text-white text-sm flex-1"
-          />
-          <GoSearch />
-        </div>
-        <div className="pl-16">
-          <button className="button px-3 py-1 text-sm">
-            ADD
-          </button>
+        <div className="w-full sm:w-auto text-center">
+          <button className="button w-full sm:w-auto px-4 py-2 text-sm">ADD</button>
         </div>
       </div>
 
-      {/* Run Button */}
       <div className="flex justify-end py-2">
-          <button className="button px-5 py-1 text-sm w-full sm:w-auto">
-          RUN SCREEN
-        </button>
+        <button className="button w-full xl:w-auto px-4 py-2 text-sm">RUN SCREEN</button>
       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
@@ -231,34 +337,47 @@ function CriteriaRow({ index, removeItem }) {
   ];
 
   return (
-    <div className="flex items-center border-b border-gray-700 py-2 gap-8">
-      <div className="flex items-center gap-2 text-3xl ">
-        <span className="text-sm">{index}</span>
- <p className="font-medium ">
-        {index === 3 ? "" : "["}
-          </p>      </div>
-      <div className={`flex items-center gap-4 ${index === 3 ? "ml-2.5" : ""}`}>
-  {index === 1 && <HiArrowSmallDown className="" />}
-  {index === 2 && <RiArrowUpDownLine className="" />}
-  {index === 3 && <HiArrowUp className="" />}
+    <div className="flex items-center border-b 2xl:bg-black borderColorCriteria py-1 pl-0 sm:pl-5  gap-2 sm:gap-8">
+      <div className="flex items-center gap-2  ">
 
-  <span className="w-5">{index === 1 ? "" : "AND"}</span>
-</div>
-      <div className="relative inline-block gap-2 px-7">
-      {index === 1 ?<FaCaretDown className="top-1 absolute pointer-events-none hidden" />  : <FaCaretDown className="top-1 absolute pointer-events-none " /> }
-        <select defaultValue={criterions[index -1]} className="appearance-none pl-8 bg-none">
+        <span className="fontInter hidden sm:block">{index}</span>
+        <p className=" text-2xl md:text-xl sm:text-lg hidden md:block">{index === 3 ? "" : "["}</p>{" "}
+      </div>
+      <div className={`md:flex items-center gap-4 hidden ${index === 3 ? "ml-1.5" : ""} ${index === 1 ? "ml-1.5" : ""}`}>
+        {index === 1 && <IoIosArrowRoundDown className="text-3xl  " />}
+        {index === 2 && <span className=" flex -gap-10 text-xl"><IoIosArrowRoundDown className="text-  " /><IoIosArrowRoundUp className="relative -left-3 -top-2" /></span>}
+        {index === 3 && <IoIosArrowRoundUp className="text-2xl" />}
+
+        <span className={`w-5  ${index === 3 ? "ml-4.5" : ""}`}>{index === 1 ? <p className=""></p> : "And"}</span>
+      </div>
+      <div className=" relative gap-0 ">
+        {index === 1 ? (
+          <FaCaretDown className="top-1 absolute pointer-events-none hidden  " />
+        ) : (
+          <FaCaretDown className="top-1 absolute pointer-events-none hidden sm:block" />
+        )}
+        <select
+          defaultValue={criterions[index - 1]}
+          className={`appearance-none pl-0 sm:pl-8  ${index === 1 ? " ml-0 sm:ml-1.5 " : ""}`}
+        >
           {criterions.map((criterion, i) => (
-            <option className="text-black" key={i} value={criterion}>
+            <option className={``} key={i} value={criterion}>
               {criterion}
             </option>
           ))}
         </select>
       </div>
-      <div className="grow flex gap-2 justify-end items-center mx-4">
-  <span> 
-  {index === 1 && <p className=""><span className=" text-sm px-1 opacity-65">{">  "}</span> 250,000</p>}
-  {index === 2 && "214,712"}
-  {index === 3 && "3,609"}</span>        <button onClick={() => removeItem(1)}>
+      <div className="grow hidden sm:flex gap-2 justify-end items-center ">
+        <span>
+          {index === 1 && (
+            <p className=" flex items-center">
+              <span className="  px-1 opacity-65  hidden 2xl:block">{">  "}</span> 250,000
+            </p>
+          )}
+          {index === 2 && "214,712"}
+          {index === 3 && "3,609"}
+        </span>{" "}
+        <button onClick={() => removeItem(1)}>
           <FaTimes />
         </button>
       </div>
